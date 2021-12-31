3c122553d4cc1d574636cf2b8de935ca83c247dc
Cole Ogrodnick <patfan8@comcast.net>
2021-12-31 17:44:52 -0500

    css

--- src/components/FlightView.js
+++ src/components/FlightView.js
@@ -5,6 +5,7 @@ const FlightView = () => {
   return (
     <div id="app-wrap-view">
       <div className="flight-ticket-wrap">
+        <div id="back-to-list-btn"></div>
         <div className="flight-ticket">
           <div id="mission-name-ticket" className="ticket-info">
             <span>Mission Name</span>
