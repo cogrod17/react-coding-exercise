ff58c530cf87ff8a02c1a203b649c435be118027
Cole Ogrodnick <patfan8@comcast.net>
2021-12-31 11:32:29 -0500

    sorting

--- src/api/index.js
+++ src/api/index.js
@@ -1,14 +1,25 @@
 import { gql } from "@apollo/react-hooks";
 
-export const getLaunchesPast = gql`
-  {
-    launchesPast(limit: 10) {
-      mission_name
-      rocket {
-        rocket_name
-        rocket_type
+let keys = {
+  "Mission Name": "mission_name",
+  "Rocket Name": "rocket_name",
+  "Rocket Type": "rocket_type",
+  "Launch Year": "launch_year",
+};
+
+export const getLaunchesPast = (order, sort, offset, limit = 10) => {
+  let ord = order === true ? "asc" : "desc";
+
+  return gql`
+    {
+      launchesPast(order: "${ord}", sort: "${keys[sort]}", offset: ${offset}, limit: ${limit}) {
+        mission_name
+        rocket {
+          rocket_name
+          rocket_type
+        }
+        launch_year
       }
-      launch_year
     }
-  }
-`;
+  `;
+};
--- src/components/MissionList.js
+++ src/components/MissionList.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Mission from "./Mission";
 import "../styles/missionlist.css";
 import { getLaunchesPast } from "../api";
-import { gql, useQuery } from "@apollo/react-hooks";
+import { useQuery } from "@apollo/react-hooks";
 
 const MissionList = () => {
-  const { loading, data, error } = useQuery(getLaunchesPast);
+  const [category, setCategory] = useState("Launch Year");
+  const [ascending, setAscending] = useState(false);
+  const [page, setPage] = useState(10);
+  const { loading, data, error } = useQuery(
+    getLaunchesPast(ascending, category, page)
+  );
 
-  if (loading) return <div>Loading...</div>;
-  console.log(data);
+  const paginate = () => {
+    setPage(page + 10);
+  };
+  const changeSort = (e) => {
+    let click = e.target.innerHTML;
+    if (category !== click) setCategory(click);
+    if (category === click) setAscending(!ascending);
+  };
 
   const renderMissions = () => {
     return data.launchesPast.map((mission, i) => {
@@ -16,19 +27,54 @@ const MissionList = () => {
     });
   };
 
+  const setClass = (tag) => {
+    let arrow = ascending ? "ascending" : "descending";
+    return tag === category ? `active ${arrow}` : "muted";
+  };
+
   return (
     <div className="mission-list-wrap">
       <div className="mission-list-head">
-        <span>Mission Name</span>
-        <span>Rocket Name</span>
-        <span>Rocket Type</span>
-        <span>Launch Year</span>
+        <span className={setClass("Mission Name")} onClick={changeSort}>
+          Mission Name
+        </span>
+        <span className={setClass("Rocket Name")} onClick={changeSort}>
+          Rocket Name
+        </span>
+        <span className={setClass("Rocket Type")} onClick={changeSort}>
+          Rocket Type
+        </span>
+        <span className={setClass("Launch Year")} onClick={changeSort}>
+          Launch Year
+        </span>
       </div>
       {loading && <div className="mission-list-loading">Loading...</div>}
-      {data && <ul className="mission-list">{data && renderMissions()}</ul>}
+      {data && (
+        <ul className="mission-list">
+          {data && renderMissions()}
+          <button className="paginate-btn">Load more</button>
+        </ul>
+      )}
       {error && <div className="mission-list-error">There was an error</div>}
     </div>
   );
 };
 
 export default MissionList;
+
+/*
+const getProperty = (obj, property) => {
+  return property === "launch_year" || property === "mission_name"
+    ? obj[property]
+    : obj.rocket[property];
+};
+console.log(data);
+  function comparator(a, b) {
+    const valA = getProperty(a, keys[category]);
+    const valB = getProperty(b, keys[category]);
+
+    if (valA < valB) return ascending ? -1 : 1;
+    if (valA > valB) return ascending ? 1 : -1;
+    return 0;
+  }
+*/
